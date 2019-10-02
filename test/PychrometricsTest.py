7982c2d1c48854a0acc5d98fd23c0a2ec3ff4698
Gregory Evans <b33rNc0d3@protonmail.com>
2019-10-01 20:31:14 -0600

    Additional test data
    New test data

--- test/PychrometricsTest.py
+++ test/PychrometricsTest.py
@@ -6,11 +6,11 @@ sys.path.append('../')
 from PyChrometrics.src.Psychrometrics import Pychrometrics
 
 def main():
-    pass
+    #pass
 
-    x = Pychrometrics( elevation = 5000 )
-    rh = 0.22
-    Tdb = 89.0
+    x = Pychrometrics( elevation = 5600 )
+    rh = 0.485
+    Tdb = 68.0
     Twb = x.T_wb_iter(Tdb, rh)
     
     print( "T_wb_reg: %.4f " %x.T_wb_regression( Tdb, rh ) )
@@ -21,22 +21,22 @@ def main():
     print( "Hum Rat f(): %.6f " %x.W1( Tdb, Twb ) )
     print( "Density %.4f " %x.density( Tdb, rh ) )
 
-    print( "Grains %.4f  " %x.grains_moisture(Tdb, rh) )
+    # print( "Grains %.4f  " %x.grains_moisture(Tdb, rh) )
 
-    print( "Sens enthalpy %.4f  " %x.enthalpy_dry_air(Tdb, rh) )
-    print( "Late enthalpy %.4f  " %x.enthalpy_vapor(Tdb, rh) )
+    # print( "Sens enthalpy %.4f  " %x.enthalpy_dry_air(Tdb, rh) )
+    # print( "Late enthalpy %.4f  " %x.enthalpy_vapor(Tdb, rh) )
     
-    print( "Enthalpy: %.2f " %x.enthalpy(Tdb, rh) )
+    # print( "Enthalpy: %.2f " %x.enthalpy(Tdb, rh) )
 
-    print( "Press atm: %.4f  " %x.P_atm_std() )
+    # print( "Press atm: %.4f  " %x.P_atm_std() )
 
-    print( "Pw_sat: %.4f  " %x.P_w_sat(Tdb) )
-    print( "Pv_sat: %.4f  " %x.P_v_sat(Tdb) )
+    # print( "Pw_sat: %.4f  " %x.P_w_sat(Tdb) )
+    # print( "Pv_sat: %.4f  " %x.P_v_sat(Tdb) )
 
     e = x.P_v_partial(Tdb,rh)
     es = x.P_w_sat(Tdb)
     
-    print("RH: %.4f "%(e/es * 100) )
+    # print("RH: %.4f "%(e/es * 100) )
 
 if __name__ == "__main__": 
     main()
